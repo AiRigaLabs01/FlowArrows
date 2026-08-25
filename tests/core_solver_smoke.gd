b42bf9eb9f89b0877b1a5513969dd62e175ed167
airiga1897 <airiga1897@gmail.com>
2026-08-25 05:56:54 +0300

    Lock in thread-sliding exit behavior

--- tests/core_solver_smoke.gd
+++ tests/core_solver_smoke.gd
@@ -7,8 +7,9 @@ const SolverScript = preload("res://src/core/solver.gd")
 func _initialize() -> void:
 	_test_single_piece()
 	_test_dependency_chain()
-	_test_bent_piece_can_exit_as_rigid_shape()
-	_test_bent_piece_blocked_during_translation()
+	_test_bent_piece_can_exit()
+	_test_bent_piece_blocked_on_head_path()
+	_test_thread_slide_avoids_rigid_translation_collision()
 	print("FlowArrows core smoke tests passed")
 	quit()
 
@@ -25,16 +26,23 @@ func _test_dependency_chain() -> void:
 	assert(board.legal_moves() == ["B"])
 	assert(SolverScript.new().solve(board) == ["B", "A"])
 
-func _test_bent_piece_can_exit_as_rigid_shape() -> void:
+func _test_bent_piece_can_exit() -> void:
 	var a = PieceScript.new("A", [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)], Vector2i.RIGHT)
 	var board = BoardScript.new(4, 4, [a])
 	assert(board.can_exit("A"))
+	assert(board.exit_steps("A") > 0)
 	assert(SolverScript.new().solve(board) == ["A"])
 
-func _test_bent_piece_blocked_during_translation() -> void:
+func _test_bent_piece_blocked_on_head_path() -> void:
 	var a = PieceScript.new("A", [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)], Vector2i.RIGHT)
 	var blocker = PieceScript.new("B", [Vector2i(3, 2)], Vector2i.DOWN)
 	var board = BoardScript.new(4, 4, [a, blocker])
 	assert(not board.can_exit("A"))
 	assert(board.can_exit("B"))
 	assert(SolverScript.new().solve(board) == ["B", "A"])
+
+func _test_thread_slide_avoids_rigid_translation_collision() -> void:
+	var a = PieceScript.new("A", [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)], Vector2i.RIGHT)
+	var blocker = PieceScript.new("B", [Vector2i(2, 1)], Vector2i.UP)
+	var board = BoardScript.new(4, 4, [a, blocker])
+	assert(board.can_exit("A"))
